419885c1fd59dfbb54cddc7047866d78999e2e74
yasmeenbaiji <158083299+yasmeenbaiji@users.noreply.github.com>
2025-11-06 15:22:32 -0500

    Update comp.js

--- comp.js
+++ comp.js
@@ -3,6 +3,7 @@ const complement = {
   T: "A",
   G: "C",
   C: "G",
+  U: "A",
 }
 
 function getComplement(seq) {
@@ -13,24 +14,24 @@ function getComplement(seq) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form")
+  const btn    = document.getElementById("enter_button_pairs"); 
   const input = document.getElementById("seqInput")
   const output = document.getElementById("output")
   const btnToRNA = document.getElementById('toRNA'); // toggle to RNA
   const btnToDNA = document.getElementById('toDNA'); // toggle to DNA
 
-  if (!form || !input || !output) return
+  if (!btn || !input || !output) return
 
-  form.addEventListener("submit", (e) => {
+  btn.addEventListener("click", (e) => {
     e.preventDefault()
 
-    const raw = (input.value || "").toUpperCase().slice(0, 23)
-    const valid = /^[ATGCU]{1,23}$/.test(raw) && !(raw.includes('T') && raw.includes('U'));
+    const raw = (input.value || "").toUpperCase().slice(0, 32)
+    const valid = /^[ATGCU]{1,32}$/.test(raw) && !(raw.includes('T') && raw.includes('U'));
 
     if (valid) {
       output.textContent = getComplement(raw)
     } else {
-      output.textContent = "Invalid sequence entered. Enter a sequence up to 32 characters with A, T, G, or C."
+      output.textContent = "Invalid sequence entered. Enter a sequence up to 23 characters with A, T, G, or C."
     }
   });
   
